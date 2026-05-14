e7df503f83fdc222cc383bcbc04a248c95312a43
Matthew Wilding <mbwilding@gmail.com>
2026-05-14 18:43:54 +0800

    Build settings

--- Source/TestEditor.Target.cs
+++ Source/TestEditor.Target.cs
@@ -8,8 +8,8 @@ public class TestEditorTarget : TargetRules
     public TestEditorTarget( TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.Add("Test");
     }
 }
